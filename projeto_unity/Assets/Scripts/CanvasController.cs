b39064129b278c1d7d5af369a8d560a1f7495d75
nandoteless <20223030993@estudantes.ifpr.edu.br>
2025-07-15 15:45:01 -0300

    Sons

--- projeto_unity/Assets/Scripts/CanvasController.cs
+++ projeto_unity/Assets/Scripts/CanvasController.cs
@@ -3,68 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using FMODUnity;
 
 public class CanvasController : MonoBehaviour
 {
+    [Header("FMOD")]
+    [SerializeField] private EventReference som;
+
     public Button botaoIniciar;
     public Button botaoJogar;
     public Button botaoHistoria;
     public Button botaoMenu;
     public Button botaoInstrucao;
 
-
     void Start()
     {
-        // Adiciona o listener ao bot�o se estiver atribu�do no Inspector
         if (botaoIniciar != null)
-        {
             botaoIniciar.onClick.AddListener(CarregarMenu);
-        }
 
-         if (botaoJogar != null)
-        {
+        if (botaoJogar != null)
             botaoJogar.onClick.AddListener(CarregarFase1);
-        }
 
-          if (botaoHistoria != null)
-        {
+        if (botaoHistoria != null)
             botaoHistoria.onClick.AddListener(CarregarHistoria);
-        }
 
-         if (botaoMenu != null)
-        {
+        if (botaoMenu != null)
             botaoMenu.onClick.AddListener(CarregarVoltar);
-        }
 
-         if (botaoInstrucao != null)
-        {
+        if (botaoInstrucao != null)
             botaoInstrucao.onClick.AddListener(CarregarInstrucao);
-        }
     }
 
     public void CarregarMenu()
     {
-        // Carrega a cena chamada "Menu"
+        RuntimeManager.PlayOneShot(som, transform.position);
         SceneManager.LoadScene("Menu");
     }
 
-      public void CarregarFase1()
+    public void CarregarFase1()
     {
+        RuntimeManager.PlayOneShot(som, transform.position);
         SceneManager.LoadScene("Cuts1");
     }
 
-     public void CarregarHistoria()
+    public void CarregarHistoria()
     {
+        RuntimeManager.PlayOneShot(som, transform.position);
         SceneManager.LoadScene("Historia");
     }
 
-     public void CarregarVoltar()
+    public void CarregarVoltar()
     {
+        RuntimeManager.PlayOneShot(som, transform.position);
         SceneManager.LoadScene("Menu");
     }
 
-      public void CarregarInstrucao()
+    public void CarregarInstrucao()
     {
+        RuntimeManager.PlayOneShot(som, transform.position);
         SceneManager.LoadScene("Instrucao");
     }
 }
