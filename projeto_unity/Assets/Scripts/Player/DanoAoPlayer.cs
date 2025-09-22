efb23a45b9ced36aa92186f19dbfb5a8030b4706
LauraGessly <20223029885@estudantes.ifpr.edu.br>
2025-09-22 11:31:05 -0300

    coisitas

---
+++ projeto_unity/Assets/Scripts/Player/DanoAoPlayer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DanoAoPlayer : MonoBehaviour
+{
+    public float dano = 25f;          // quanto dano o player leva
+    public float intervaloDano = 1f;  // tempo entre danos
+
+    private float tempoUltimoDano = 0f;
+    private PlayerVida playerVida;
+
+    void Start()
+    {
+        playerVida = GetComponent<PlayerVida>();
+        if (playerVida == null)
+        {
+            Debug.LogError("PlayerVida não encontrado no Player!");
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        // Se encostar em inimigo (tag "Inimigo") e já passou o intervalo
+        if (collision.gameObject.CompareTag("onca") && Time.time - tempoUltimoDano >= intervaloDano)
+        {
+            if (playerVida != null)
+            {
+                playerVida.ReduzirVida(dano);
+            }
+            tempoUltimoDano = Time.time;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        // Também funciona se inimigo usar trigger
+        if (other.CompareTag("onca") && Time.time - tempoUltimoDano >= intervaloDano)
+        {
+            if (playerVida != null)
+            {
+                playerVida.ReduzirVida(dano);
+            }
+            tempoUltimoDano = Time.time;
+        }
+    }
+}
